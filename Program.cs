15758914dc849664bd461e64ad08e82ae0d88626
Vasilisa-Viktoria <66121101+VasilisaViktoriya@users.noreply.github.com>
2021-08-25 20:20:28 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, geek ");
+            Console.ReadLine();
+        }
+    }
+}
